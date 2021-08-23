5ecd1ae84f9dc5d1d7836c682b6ab91c0ab9921a
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-23 12:33:02 +0300

    feat(style): add tailwindcss [no:ref]
    * install tailwindcss
     * add tailwindcss in globals.css

---
+++ pages/to-do/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div >
+        <h1 className="text-grey-darkest">Todo List</h1>
+    </div>
+  )
+}
+
+export default Home
