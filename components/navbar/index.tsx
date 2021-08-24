8bfb484e82489d342c207365ef9c3e099c62ebe9
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-24 18:20:47 +0300

    fix(component): fix navlink[no:ref]
    * change in navlink 'home' -> '/home'

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -4,7 +4,7 @@ import {NavLink} from "../nav-link";
 export const Navbar = () => {
     return (
         <div className="text-md font-bold text-black lg:flex-grow">
-            <NavLink href="home"
+            <NavLink href="/home"
                      className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-300 mr-2">
                 Home
             </NavLink>
