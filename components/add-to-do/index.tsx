ee53c4d552eb45af9963205cb025ae631ae83690
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-24 13:34:08 +0300

    feat(component): add todo [no:ref]
    * add feature "add todo"

--- components/add-to-do/index.tsx
+++ components/add-to-do/index.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export const AddToDo = () => {
+import {ToDoApi} from "../../lib/service/todo-api.service";
+import {useToDoContext} from "../../lib/context/todo-context";
+import {IToDo} from "../../lib/interfaces/to-do.interface";
+
+
+export const AddToDo:React.FC = () => {
+
+    const [title, setTitle] = useState<string>('')
+    const [isTodoSending, setIsTodoSending] = useState<boolean>(false)
+    const [todoSendingFailure, setTodoSendingFailure] = useState<string | string[] | null>(null)
+    const {todos,setTodos} = useToDoContext()
+
+    const onChangeTodo = (e: React.FormEvent<HTMLInputElement>) => {
+        if (title.length < 30) {
+            setTitle(e.currentTarget.value)
+        }
+        return
+    }
+
+    const addTodo = async () => {
+        try {
+            if(!title.length){
+                return
+            }
+
+
+            setIsTodoSending(true)
+            const response = await ToDoApi.addTodo({title})
+            console.log(response)
+            setTodos(((prevState: IToDo[]) => {
+                if (prevState && prevState.length >= 1) {
+                    return [response, ...prevState]
+                }
+                return [response]
+            }))
+            console.log(todos)
+        } catch (e) {
+            setTodoSendingFailure(e)
+        } finally {
+            setIsTodoSending(false)
+        }
+    }
+
+
+
+
     return (
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+            {todoSendingFailure
+                ? <pre>{todoSendingFailure}</pre>
+                : ''
+            }
             <div className="mb-4">
                 <h1 className="text-grey-darkest">Todo List</h1>
                 <div className="flex mt-4">
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                    <input disabled={isTodoSending} onChange={onChangeTodo}
+                           className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
                            placeholder="Add Todo"/>
-                    <button
-                        className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add
+                    <button onClick={addTodo}
+                            className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add
                     </button>
                 </div>
             </div>
--- components/to-do-item/index.tsx
+++ components/to-do-item/index.tsx
@@ -1,9 +1,17 @@
-import React, { useState} from 'react';
+import React, {useState} from 'react';
+
+import {IResponseMessage} from "../../lib/interfaces/response-message.interface";
+import {useToDoContext} from "../../lib/context/todo-context";
+import {IToDo} from "../../lib/interfaces/to-do.interface";
+import {ToDoApi} from "../../lib/service/todo-api.service";
 import TodoStatus from "../todo-status";
 
-export const ToDoItem = () => {
+export const ToDoItem: React.FC<IToDo> = ({title, status, id}) => {
+
+    const {setTodos} = useToDoContext()
+
     const [isNeedShowStatus, setIsNeedShowStatus] = useState<boolean>(false)
-  //  const [currentStatus, setCurrentStatus] = useState<string>('in progress')
+    //  const [currentStatus, setCurrentStatus] = useState<string>('in progress')
 
 
     const closeStatusBar = () => {
@@ -11,8 +19,10 @@ export const ToDoItem = () => {
         setIsNeedShowStatus(false)
     }
 
-    const opeStatusBar = (e:MouseEvent) => {
+    const opeStatusBar = (e: MouseEvent) => {
+
         e.stopPropagation()
+
         setIsNeedShowStatus(true)
 
     }
@@ -26,20 +36,38 @@ export const ToDoItem = () => {
         };
     }, []);
 
+    const removeTodo = async () => {
+        try {
+            const deleted: IResponseMessage = await ToDoApi.removeTodo(id)
+            console.log(deleted)
+            if (deleted.message === 'success') {
+                setTodos((prevState: IToDo[]) => {
+                    return prevState.filter(todo => todo.id !== id)
+                })
+            }
+        } catch (e) {
+            //setSomeError
+            console.log(e)
+        } finally {
+            //loader false
+        }
+    }
+
 
     return (
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
 
             <div>
                 <div className="flex mb-4 items-center">
-                    <p className="w-full  text-green">Submit To-do App Component to Tailwind Components</p>
+                    <p className="w-full  text-green">{title}</p>
+                    {/*// @ts-ignore*/}
                     <button onClick={opeStatusBar}
                             className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">
-                        Status
+                        {status}
                     </button>
                     {isNeedShowStatus ? <TodoStatus/> : ''}
 
-                    <button
+                    <button onClick={removeTodo}
                         className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove
                     </button>
                 </div>
