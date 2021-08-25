04c66b328f1c41487db829046148a9ea4716d7b9
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-25 13:17:38 +0300

    fix(deploy): change path endpoint [no:ref]
    * change path endpoint

--- lib/service/todo-api.service.ts
+++ lib/service/todo-api.service.ts
@@ -7,7 +7,7 @@ export class ToDoApi {
 
 
     static async getTodos(query?: string): Promise<ITodoResponse> {
-        const uri = query ? 'to-do?' + query : 'to-do'
+        const uri = query ? 'to-do/[[...params]]?' + query : 'to-do/[[...params]]'
 
         const {data} = await axios$.get<ITodoResponse>(uri);
 
@@ -15,17 +15,17 @@ export class ToDoApi {
     }
 
     static async addTodo(payload: Partial<Omit<IToDo, 'id'>>): Promise<IToDo> {
-        const {data} = await axios$.post<IToDo>('to-do', payload);
+        const {data} = await axios$.post<IToDo>('to-do/[[...params]]', payload);
         return data;
     }
 
     static async removeTodo(id: number): Promise<IResponseMessage> {
-        const {data} = await axios$.delete<IResponseMessage>(`to-do/${id}`);
+        const {data} = await axios$.delete<IResponseMessage>(`to-do/[[...params]]/${id}`);
         return data;
     }
 
     static async updateTodo(id:number,payload: Partial<Omit<IToDo, 'id'>>): Promise<IResponseMessage> {
-        const {data} = await axios$.put<IResponseMessage>(`to-do/${id}`, payload);
+        const {data} = await axios$.put<IResponseMessage>(`to-do/[[...params]]/${id}`, payload);
         return data;
     }
 
