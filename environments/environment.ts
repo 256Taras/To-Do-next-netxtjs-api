ec2a0187a1f5e819a057ca8e748935ccac9d3b3d
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-25 23:04:36 +0300

    feat(ws): change environment [no:ref]

--- environments/environment.ts
+++ environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
         dropSchema: false,
     },
     baseURL: isProd
-        ? 'https://to-do-next-netxtjs-api.vercel.app/'
+        ? 'https://to-do-next-netxtjs-api.vercel.app/api/v1/'
         : 'http://localhost:3000/api/v1/'
 
 
