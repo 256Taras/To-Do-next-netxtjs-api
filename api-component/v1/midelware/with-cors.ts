d272045f2b05490f5ef2b0ee68ad2e4cebb8b1b3
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-25 22:54:21 +0300

    feat(api): delete next-cors [no:ref]

--- api-component/v1/midelware/with-cors.ts
+++ api-component/v1/midelware/with-cors.ts
@@ -1,5 +1,4 @@
-
-import NextCors from 'nextjs-cors';
+import Cors from 'cors'
 
 import {NextApiRequest, NextApiResponse} from 'next';
 
@@ -7,17 +6,32 @@ import {NextApiRequest, NextApiResponse} from 'next';
 export type Handler = (req: NextApiRequest, res: NextApiResponse) => void;
 
 function initCors() {
-    return async (req: NextApiRequest, res: NextApiResponse) =>
+    return (req: NextApiRequest, res: NextApiResponse) =>
+        new Promise((resolve, reject) => {
 
-        await NextCors(req, res, {
-            // Options
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-            origin: 'https://test-todo-api-884.herokuapp.com',
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-            credentials: true,
-            allowedHeaders: 'Content-Type, Authorization,Accept,X-Requested-With',
+            Cors({
+                origin: 'https://test-todo-api-884.herokuapp.com',
+                methods: 'GET, POST, PUT, PATCH, POST, DELETE,OPTIONS, HEAD',
+                allowedHeaders: 'Content-Type, Authorization,Accept,X-Requested-With',
+                credentials: true,
+
+            })(req, res, (result: unknown) => {
+
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader(
+                    "Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+                );
+                if (req.method == "OPTIONS") {
+                    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+                    return res.status(200).json({});
+                }
+                if (result instanceof Error) {
+                    return reject(result);
+                }
+                return resolve(result);
+            });
         });
-
 }
 
 export const withCors = (handler: Handler) => async (req: NextApiRequest, res: NextApiResponse) => {
